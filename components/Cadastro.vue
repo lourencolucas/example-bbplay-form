d0481d042fedea72922d8b70a17d9eb8cc24a72d
unknown <lucaslou189@gmail.com>
2021-07-17 12:43:15 -0300

    Arquivos do projeto

---
+++ components/Cadastro.vue
@@ -0,0 +1,173 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group>
+        <b-form-row>
+          <b-col cols="6"
+            ><b-form-input placeholder="Nome" v-model="form.name" required
+          /></b-col>
+          <b-col
+            ><b-form-input
+              placeholder="CPF"
+              v-model="form.cpf"
+              required
+              maxlength="11"
+          /></b-col>
+          <b-col
+            ><b-form-input
+              placeholder="Data de Nascimento"
+              type="date"
+              v-model="form.birthdate"
+              required
+          /></b-col>
+        </b-form-row>
+      </b-form-group>
+      <b-form-group>
+        <b-form-row>
+          <b-col
+            ><b-form-input placeholder="CEP" v-model="form.cep" required
+          /></b-col>
+          <b-col cols="10"
+            ><b-form-input
+              placeholder="Endereço"
+              v-model="form.address"
+              required
+          /></b-col>
+        </b-form-row>
+      </b-form-group>
+      <b-form-group>
+        <b-form-row>
+          <b-col cols="6"
+            ><b-form-input placeholder="Complemento" v-model="form.address2"
+          /></b-col>
+          <b-col
+            ><b-form-input placeholder="Cidade" v-model="form.city" required
+          /></b-col>
+          <b-col
+            ><b-form-input placeholder="Estado" v-model="form.state" required
+          /></b-col>
+        </b-form-row>
+      </b-form-group>
+      <b-form-group>
+        <b-form-row>
+          <b-col>
+            <b-form-input
+              placeholder="Email"
+              type="email"
+              required
+              v-model="form.email"
+            />
+          </b-col>
+          <b-col>
+            <b-form-input
+              placeholder="Confirmar Email"
+              type="email"
+              required
+              v-model="form.emailcheck"
+            />
+          </b-col>
+        </b-form-row>
+      </b-form-group>
+      <b-form-group>
+        <b-form-row>
+          <b-col>
+            <b-form-input
+              placeholder="Senha"
+              type="password"
+              required
+              v-model="form.password"
+            />
+          </b-col>
+          <b-col>
+            <b-form-input
+              placeholder="Confirmar Senha"
+              type="password"
+              required
+              v-model="form.passwordcheck"
+            />
+          </b-col>
+        </b-form-row>
+      </b-form-group>
+      <b-form-group id="input-check" v-slot="{ ariaDescribedby }">
+        <b-form-checkbox-group
+          v-model="form.checked"
+          required
+          id="checkboxes"
+          :aria-describedby="ariaDescribedby"
+        >
+          <b-form-checkbox value="yes" class="letter"
+            >Eu concordo com a
+            <a href="https://boxbrazilplay.tv.br/privacidade.html"
+              >Politica de Privacidade</a
+            >
+            e os Termos de Uso.</b-form-checkbox
+          >
+        </b-form-checkbox-group>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Cadastrar</b-button>
+      <b-button type="reset" variant="warning">Limpar</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        emailcheck: "",
+        password: "",
+        passwordcheck: "",
+        name: "",
+        cpf: "",
+        birthdate: "",
+        cep: "",
+        address: "",
+        address2: "",
+        state: "",
+        city: "",
+        checked: [],
+      },
+      show: true,
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.emailcheck = "";
+      this.form.password = "";
+      this.form.passwordcheck = "";
+      this.form.name = "";
+      this.form.cpf = "";
+      this.form.birthdate = "";
+      this.form.cep = "";
+      this.form.address = "";
+      this.form.address2 = "";
+      this.form.state = "";
+      this.form.city = "";
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.letter {
+  color: var(--light);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <body>
+    <b-container class="bv-example-row vh-10">
+      <b-row class="text-center">
+        <b-col cols="12">
+          <b-img src="~/static/boxbrazil-logo.png" style="max-width: 400px"></b-img>
+          <h1>BEM-VINDO À BOX BRAZIL PLAY!</h1>
+        </b-col>
+      </b-row>
+      <b-row class="text-center info-b">
+        <b-col cols="12">
+          Cadastre-se abaixo e tenha acesso aos catálogos disponíveis para
+          assinatura.
+          <br />
+          Ah, não se preocupe. Este cadastro é gratuito!
+        </b-col>
+      </b-row>
+      <b-row class="text-center info-b">
+        <b-col cols="12">
+          Para conhecer um pouco mais sobre a diversidade da produção
+          nacional,<br />
+          após conclusão do cadastro, escolha seu plano.
+        </b-col>
+      </b-row>
+      <b-row class="text-center info-b">
+        <b-col cols="12">
+          Ficou com alguma dúvida?
+          <br />
+        </b-col>
+      </b-row>
+      <b-row class="text-center">
+        <b-col cols="12">
+          <b-button variant="info" href="http://boxbrazilplay.tv.br/">
+            CLIQUE AQUI!
+          </b-button>
+        </b-col>
+      </b-row>
+      <b-row class="text-center">
+        <b-col cols="12"> <h1>CADASTRE-SE GRATUITAMENTE</h1> </b-col>
+      </b-row>
+      <Cadastro />
+      <!-- <Data/> -->
+    </b-container>
+  </body>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+body {
+  background-image: url("/bg-form-bbplay.jpg");
+  margin: none;
+}
+
+.info-b {
+  color: var(--light);
+}
+
+h1 {
+  color: var(--light);
+}
+</style>
